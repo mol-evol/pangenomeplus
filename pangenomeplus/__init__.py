5b684a1bab913797d9552185d05e7ff86ba07d94
mol-evol <mcinerney.james+panGPT@gmail.com>
2025-10-07 15:02:00 +0100

    Bump version to 0.2.0 for Bakta GFF support

--- pangenomeplus/__init__.py
+++ pangenomeplus/__init__.py
@@ -5,6 +5,6 @@ A bioinformatics tool for analyzing all genomic features across multiple genomes
 including protein-coding genes, intergenic regions, tRNAs, rRNAs, and CRISPR arrays.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "James McInerney"
 __github__ = "https://github.com/mol-evol/pangenomeplus"
